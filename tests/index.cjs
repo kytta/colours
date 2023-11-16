516078c80978601a2b3c41203df1733e99cfcef2
Nikita Karamov <me@kytta.dev>
2023-11-16 21:17:02 +0100

    Move all tests to use suites

--- tests/index.cjs
+++ tests/index.cjs
@@ -1,9 +1,20 @@
-const {test} = require('uvu');
+const {suite} = require('uvu');
 const assert = require('uvu/assert');
 const colours = require('../index.cjs');
 
-test('main', () => {
-	assert.equal(colours.red('foo'), '\u001B[31mfoo\u001B[39m');
+const cjs = suite('CommonJS build');
+
+cjs('formats strings', () => {
+	assert.equal(colours.cyan('foo'), '\u001B[36mfoo\u001B[39m');
 });
 
-test.run();
+cjs('can be nested', () => {
+	assert.equal(colours.bold(colours.cyan('foo')), '\u001B[1m\u001B[36mfoo\u001B[39m\u001B[22m');
+});
+
+cjs('can be complexly nested', () => {
+	assert.equal(`${colours.bold('foo')} bar ${colours.cyan('baz')}`,
+		'\u001B[1mfoo\u001B[22m bar \u001B[36mbaz\u001B[39m');
+});
+
+cjs.run();
--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,20 @@
-import {test} from 'uvu';
+import {suite} from 'uvu';
 import * as assert from 'uvu/assert';
 import * as colours from '../index.js';
 
-test('main', () => {
-	assert.equal(colours.red('foo'), '\u001B[31mfoo\u001B[39m');
+const esm = suite('ESM build');
+
+esm('formats strings', () => {
+	assert.equal(colours.cyan('foo'), '\u001B[36mfoo\u001B[39m');
 });
 
-test.run();
+esm('can be nested', () => {
+	assert.equal(colours.bold(colours.cyan('foo')), '\u001B[1m\u001B[36mfoo\u001B[39m\u001B[22m');
+});
+
+esm('can be complexly nested', () => {
+	assert.equal(`${colours.bold('foo')} bar ${colours.cyan('baz')}`,
+		'\u001B[1mfoo\u001B[22m bar \u001B[36mbaz\u001B[39m');
+});
+
+esm.run();
